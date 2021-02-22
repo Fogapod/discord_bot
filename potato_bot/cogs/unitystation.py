9491bfd363c9de42527a555085924bdd97b114e3
Eugene <gish.ee18@gmail.com>
2021-02-23 00:19:39 +0300

    Cleanup and rearrange servers command code

--- potato_bot/cogs/unitystation.py
+++ potato_bot/cogs/unitystation.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Any, Dict
 
 from discord.ext import commands
 
@@ -14,76 +14,90 @@ class UnityStation(commands.Cog):
     You can learn more about this game by joining their official server: discord.gg/tFcTpBp
     """
 
-    @commands.command(aliases=["list"])
-    async def servers(self, ctx: Context, *, server_name: str = None):
+    @commands.command(aliases=["list", "sv"])
+    async def servers(self, ctx: Context, *, server: str = None):
         """List hub servers"""
 
         async with ctx.typing():
-            await self._servers(ctx, server_name)
+            if server is None:
+                await self._servers(ctx)
+            else:
+                await self._server(ctx, server)
 
-    async def _servers(self, ctx, server_name: Optional[str]):
+    async def _fetch_servers(self, ctx: Context) -> Dict[str, Any]:
         async with ctx.session.get("https://api.unitystation.org/serverlist") as r:
+            if r.status != 200:
+                await ctx.send(f"Bad API response status code: {r.status}", exit=True)
+
             # they send json with html mime type
             data = await r.json(content_type=None)
 
-        servers = data["servers"]
-        if not servers:
-            return await ctx.send("No servers online")
-
-        if server_name is not None:
-            server_name = server_name.lower()
-
-            for server in servers:
-                if server["ServerName"].lower() == server_name:
-                    break
-            else:
-                return await ctx.send("No servers with this name")
-
-            # basic info
-            aliases = {
-                "name": "ServerName",
-                "fork": "ForkName",
-                "build": "BuildVersion",
-                "map": "CurrentMap",
-                "gamemode": "GameMode",
-                "time": "IngameTime",
-                "players": "PlayerCount",
-                "fps": "fps",
-            }
-            longest_alias = len(sorted(aliases.keys(), key=lambda x: len(x))[-1])
-
-            result = "\n".join(
-                f"{alias:<{longest_alias}} : {server.get(key, 'unknown')}"
-                for alias, key in aliases.items()
-            )
-
-            # compose address from 2 variables
-            ip = server.get("ServerIP", "unknown")
-            port = server.get("ServerPort", "unknown")
-
-            result += f"\n{'address':<{longest_alias}} : {ip}:{port}\n"
-
-            # downloads
-            result += "\nDownloads\n"
-            download_aliases = {
-                "windows": "WinDownload",
-                "linux": "LinuxDownload",
-                "osx": "OSXDownload",
-            }
-            longest_download_alias = len(
-                sorted(download_aliases.keys(), key=lambda x: len(x))[-1]
-            )
-
-            result += "\n".join(
-                f"{alias:<{longest_download_alias}} : {server.get(key, 'unknown')}"
-                for alias, key in download_aliases.items()
-            )
-
-            return await ctx.send(f"```\n{result}```")
-
         # sort using player count and name fields, player count is reversed and is more
         # significant
-        servers.sort(key=lambda x: (-x["PlayerCount"], x["ServerName"]))
+        return sorted(
+            data["servers"], key=lambda s: (-s["PlayerCount"], s["ServerName"])
+        )
+
+    async def _server(self, ctx: Context, server_name: str):
+        servers = await self._fetch_servers(ctx)
+
+        server_name = server_name.lower()
+
+        for server in servers:
+            if server["ServerName"].lower().startswith(server_name):
+                break
+        else:
+            return await ctx.send("No servers with this name")
+
+        download_aliases = {
+            "WinDownload": "windows",
+            "LinuxDownload": "linux",
+            "OSXDownload": "osx",
+        }
+
+        longest_download_name = len(
+            max(download_aliases.values(), key=lambda v: len(v))
+        )
+
+        downloads = "\n".join(
+            f"{alias:<{longest_download_name}} : {server.pop(name)}"
+            for name, alias in download_aliases.items()
+        )
+
+        # compose address from 2 variables
+        ip = server.pop("ServerIP", "unknown")
+        port = server.pop("ServerPort", "unknown")
+
+        server["address"] = f"{ip}:{port}"
+
+        aliases = {
+            "ServerName": "name",
+            "ForkName": "fork",
+            "BuildVersion": "build",
+            "CurrentMap": "map",
+            "GameMode": "gamemode",
+            "IngameTime": "time",
+            "PlayerCount": "players",
+        }
+        longest_name = len(max(server.keys(), key=lambda k: len(aliases.get(k, k))))
+
+        main_info = "\n".join(
+            f"{aliases.get(name, name):<{longest_name}} : {value}"
+            for name, value in server.items()
+        )
+
+        await ctx.send(f"```\n{main_info}\n\nDownloads\n{downloads}```")
+
+    async def _servers(self, ctx: Context):
+        if not (servers := await self._fetch_servers(ctx)):
+            return await ctx.send("No servers online")
+
+        # newlines are allowed in server names
+        for server in servers:
+            server["ServerName"] = server.get("ServerName", "unknown").replace(
+                "\n", " "
+            )
+
         columns = {
             "Server": "ServerName",
             "Build": "BuildVersion",
@@ -108,8 +122,7 @@ class UnityStation(commands.Cog):
         for server in servers:
             entries = []
             for col_name, col_key in columns.items():
-                # newlines are allowed in server names
-                value = str(server.get(col_key, "unknown")).replace("\n", " ")
+                value = server.get(col_key, "unknown")
 
                 entries.append(f"{value:<{column_widths[col_name]}}")
 
