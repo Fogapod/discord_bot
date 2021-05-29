3e70a164bf3602d39b3dbaef1aa2be92a87115fc
Eugene <fogaprod@gmail.com>
2021-05-29 21:28:14 +0300

    Add more nya variations and remove stupid broken math

--- pink/cogs/accents/accents/owo.py
+++ pink/cogs/accents/accents/owo.py
@@ -1,5 +1,6 @@
-import math
 import random
+
+from typing import Optional
 
 from .accent import Match, Accent
 
@@ -11,6 +12,7 @@ NYAS = (
     "OwO",
     "nya",
     "Nya",
+    "nyaa",
     "nyan",
     "!!!",
     "(=^‥^=)",
@@ -19,9 +21,56 @@ NYAS = (
     "~~",
     "*wings their tail*",
     "\N{PAW PRINTS}",
+    # most of these are taken from: https://github.com/tgstation/tgstation/blob/67ec6e8daa0fc58eeda91d96468960f4ad29b5db/code/modules/language/nekomimetic.dm#L7-L11
+    "neko",
+    "mimi",
+    "moe",
+    "mofu",
+    "fuwa",
+    "kyaa",
+    "kawaii",
+    "poka",
+    "munya",
+    "puni",
+    "munyu",
+    "ufufu",
+    "uhuhu",
+    "icha",
+    "doki",
+    "kyun",
+    "kusu",
+    "desu",
+    "kis",
+    "ama",
+    "chuu",
+    "baka",
+    "hewo",
+    "boop",
+    "gato",
+    "kit",
+    "sune",
+    "yori",
+    "sou",
+    "baka",
+    "chan",
+    "san",
+    "kun",
+    "mahou",
+    "yatta",
+    "suki",
+    "usagi",
+    "domo",
+    "ori",
+    "uwa",
+    "zaazaa",
+    "shiku",
+    "puru",
+    "ira",
+    "heto",
+    "etto",
 )
 
-EXTREME_NYAS = (
+FORBIDDEN_NYAS = (
     ";)",
     "uwu",
     "UwU",
@@ -29,28 +78,42 @@ EXTREME_NYAS = (
 
 ALL_NYAS = (
     *NYAS,
-    *EXTREME_NYAS,
+    *FORBIDDEN_NYAS,
 )
 
-EXTREME_NYA_TRESHOLD = 5
+FORBIDDEN_NYA_TRESHOLD = 5
 
 
 def nya(m: Match) -> str:
     weights = [1] * len(NYAS)
 
-    if m.severity > EXTREME_NYA_TRESHOLD:
-        weights += [m.severity - EXTREME_NYA_TRESHOLD] * len(EXTREME_NYAS)
+    if m.severity > FORBIDDEN_NYA_TRESHOLD:
+        weights += [m.severity - FORBIDDEN_NYA_TRESHOLD] * len(FORBIDDEN_NYAS)
     else:
-        weights += [0] * len(EXTREME_NYAS)
+        weights += [0] * len(FORBIDDEN_NYAS)
 
-    return " ".join(
-        random.choices(ALL_NYAS, weights)[0]
-        for _ in range(random.randint(0, max((1, round(m.count_logarithmic() * 5)))))
-    )
+    max_nyas_x100 = int((0.5 + m.severity * 0.25) * 100)
+
+    count = round(random.randint(0, max_nyas_x100) / 100)
+
+    return " ".join(random.choices(ALL_NYAS, weights, k=count))
 
 
-def logarithmic_owo(s: int) -> float:
-    return max((0.25, math.log10(s)))
+# since nya is not guaranteed to produce value, using something like
+# lambda m: f"{nya(m)} "
+# may leave empty whitespace which isn't stripped
+def nya_message_start(m: Match) -> Optional[str]:
+    if value := nya(m):
+        return f"{value} "
+
+    return None
+
+
+def nya_message_end(m: Match) -> Optional[str]:
+    if value := nya(m):
+        return f" {value}"
+
+    return None
 
 
 class OwO(Accent):
@@ -67,12 +130,6 @@ class OwO(Accent):
         r"ne": "nye",
         r"no": "nyo",
         r"nu": "nyu",
-        Accent.MESSAGE_START: {
-            lambda m: f"{nya(m)} ": logarithmic_owo,
-            None: 1,
-        },
-        Accent.MESSAGE_END: {
-            lambda m: f" {nya(m)}": logarithmic_owo,
-            None: 1,
-        },
+        Accent.MESSAGE_START: nya_message_start,
+        Accent.MESSAGE_END: nya_message_end,
     }
