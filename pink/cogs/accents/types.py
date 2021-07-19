5fa807c52a30691cbde3ff85223da494ce6f9900
Eugene <fogaprod@gmail.com>
2021-07-19 17:49:22 +0300

    More robust accent conventer

--- pink/cogs/accents/types.py
+++ pink/cogs/accents/types.py
@@ -15,21 +15,20 @@ class PINKAccent(Accent, register=False):
 
     @classmethod
     async def convert(cls, ctx: Context, argument: str) -> Accent:
-        match = re.match(r"(.+?)(:?\[(\d+)\])?$", argument)
+        match = re.fullmatch(r"(.+?)(:?\[(\d{1,2})\])?", argument)
         assert match
 
-        name = match[1]
+        name = match[1].lower()
         severity = 1 if match[3] is None else int(match[3])
 
-        prepared = name.replace(" ", "_").lower()
         try:
-            accent = ALL_ACCENTS[prepared]
+            accent = ALL_ACCENTS[name.lower()]
         except KeyError:
-            raise commands.BadArgument(f'Accent "{name}" does not exist')
+            raise commands.BadArgument(f"not a valid accent: {name}")
 
         if not (cls.MIN_SEVERITY <= severity <= cls.MAX_SEVERITY):
             raise commands.BadArgument(
-                f"{accent}: severity must be between {cls.MIN_SEVERITY} and {cls.MAX_SEVERITY}"
+                f"{accent.name}: severity must be between {cls.MIN_SEVERITY} and {cls.MAX_SEVERITY}"
             )
 
         return accent(severity)
