7b41dd06ec3d02f04301fb62e810b15db15ade89
Eugene <fogaprod@gmail.com>
2023-12-26 02:56:16 +0400

    replace

--- update.sh
+++ update.sh
@@ -2,5 +2,4 @@
 
 # this is a manual scipt while i build some CD
 podman pull docker.io/fogapod/pink
-podman rm -f pink-bot
-podman run --name pink-bot --restart always -v ${PWD}/settings.toml:/code/settings.toml --hostname pink_prod --network host -d docker.io/fogapod/pink
+podman run --replace --name pink-bot --restart always -v ${PWD}/settings.toml:/code/settings.toml --hostname pink_prod --network host -d docker.io/fogapod/pink
