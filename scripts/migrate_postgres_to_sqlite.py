7bb20f92a616574617dbfa2370a7ef5afbd591e8
Eugene <fogaprod@gmail.com>
2024-04-30 15:06:54 +0400

    add postgres to sqlite migration script

---
+++ scripts/migrate_postgres_to_sqlite.py
@@ -0,0 +1,35 @@
+import asyncio
+import sqlite3
+
+from pathlib import Path
+
+import asyncpg
+
+
+async def main() -> None:
+    pg = await asyncpg.create_pool(
+        host=input("host: ") or "localhost",
+        port=int(input("port: ") or 5432),
+        user=input("user: "),
+        password=input("password: "),
+        database=input("database: "),
+    )
+    conn = sqlite3.connect("pink.db")
+    db = conn.cursor()
+
+    with Path("schema.sql").open() as f:
+        db.executescript(f.read())
+
+    prefixes = [r async for r in pg.fetch("SELECT * FROM prefixes")]
+    print("inserting", len(prefixes), "prefixes")
+    db.executemany("INSERT INTO prefixes (guild_id, prefix) VALUES (?, ?)", prefixes)
+
+    accents = [r async for r in pg.fetch("SELECT * FROM accents")]
+    print("inserting", len(accents), "accents")
+    db.executemany("INSERT INTO accents (guild_id, user_id, name, severity) VALUES (?, ?, ?, ?)", accents)
+
+    conn.commit()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
