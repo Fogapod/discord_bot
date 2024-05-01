5b1b0ad605bae60643f81a2e22e371bd10eccb86
Eugene <fogaprod@gmail.com>
2024-05-01 22:23:07 +0400

    use folder volume

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,4 +2,4 @@
 
 # this is a manual scipt while i build some CD
 podman pull docker.io/fogapod/pink
-podman run --replace --name pink-bot --restart always -v ${PWD}/settings.toml:/code/settings.toml -v pink_bot_db:/code/pink.db --hostname pink_prod --network host -d docker.io/fogapod/pink
+podman run --replace --name pink-bot --restart always -v ${PWD}/settings.toml:/code/settings.toml -v pink_bot_data:/data --hostname pink_prod --network host -d docker.io/fogapod/pink
